7cc9a3b0b927f620863de908d6efb1845ac14d17
Nav3aj <navrajamgai01@gmail.com>
2026-05-05 13:10:55 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts?limit=10')
+      .then(res => res.json())
+      .then(data => {
+        setPosts(data)
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) return <p>Loading...</p>
+
+  return (
+    <div>
+      <h1>Posts from API (By Navraj)</h1>
+      {posts.map(post => (
+        <div key={post.id} style={{border: "1px solid black", margin: "10px", padding: "10px", color: "green", borderBlockColor: "red"}}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
